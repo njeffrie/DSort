389540c92ae5c43719c0e2d6a3de17127ca08d10
Nat Jeffries <natjeffries@Nats-MacBook-Air.local>
2014-12-24 01:30:45 -0500

    server socket currently does not connect properly

---
+++ Client.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class Client {
+  public static void sendArrayList(ArrayList<Integer> list){
+    try {
+      Socket sock = new Socket("Server", 6060);
+      ObjectOutputStream ObjectOut = new ObjectOutputStream(sock.getOutputStream());
+      ObjectOut.writeObject(list);
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+
+  public static ArrayList<Integer>  getArrayList() {
+    try {
+      //ArrayList<Integer> list = new ArrayList<Integer>();
+      Socket sock = new Socket("Server", 6060);
+      ObjectInputStream ObjectIn = new ObjectInputStream(sock.getInputStream());
+      Object object = ObjectIn.readObject();
+      @SuppressWarnings("unchecked")
+      ArrayList<Integer> list = (ArrayList<Integer>) object;
+      return list;
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+    // only happens if error occurs
+    return null;
+  }
+
+  public static ArrayList<Integer> readRandomFile (String fname){
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    try {
+      Scanner s = new Scanner(new File(fname));
+      while(s.hasNextInt())
+        list.add(s.nextInt());
+      return list;
+    } 
+    catch(Exception e){
+      e.printStackTrace();
+    }
+    return null;
+  }
+  
+  public static void main(String [] args){
+    ArrayList<Integer> list = readRandomFile("random_numbers");
+    sendArrayList(list);
+    ArrayList<Integer> sortedlist = getArrayList();
+  }
+}
--- Server.java
+++ Server.java
@@ -1,62 +1,24 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.net.*;
 import java.io.*;
 import java.util.*;
-import java.net.*;
 
-
-public class Server {
-	
-	static ServerSocket ss;
-	static Socket s;
-	static BufferedReader br;
-	static BufferedWriter bw;
-	static String[] names = new String[1000];
-	static BufferedReader[] brArray = new BufferedReader[1000];
-	static BufferedWriter[] bwArray = new BufferedWriter[1000];
-	
-	public static void main(String[] args) {
-		
-		try {
-		
-			ss = new ServerSocket(8100);
-		
-			while(true) {
-				
-				s = ss.accept();
-	
-				br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-				bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-				String name = br.readLine();
-				
-				int nextSpace = 0;
-				while(names[nextSpace] != null) {
-					nextSpace++;
-				}
-				
-				names[nextSpace] = name;
-				brArray[nextSpace] = br;
-				bwArray[nextSpace] = bw;
-		
-				RTS t = new RTS(br, bw, name, names, brArray, bwArray);
-				
-				int i=0;
-				while(names[i] != null) {
-					if((i != nextSpace) && (names[i].compareTo("0")!=0)){
-						bw.write("@#$"+names[i]);
-						bw.newLine();
-						bw.flush();
-					}
-					i++;
-				}
-				bw.write("$#@");
-				bw.newLine();
-				bw.flush();
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+public class Server{
+  public static void main(String[] args) {
+    try 
+    {
+      ServerSocket myServerSocket = new ServerSocket(6060);
+      Socket skt = myServerSocket.accept();
+      ObjectInputStream in = new ObjectInputStream(skt.getInputStream());
+      Object object = in.readObject();
+      @SuppressWarnings("unchecked")
+      ArrayList<Integer> list = (ArrayList<Integer>) object;
+      Collections.sort(list);
+      ObjectOutputStream out = new ObjectOutputStream(skt.getOutputStream());
+      out.writeObject(list);
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
 }
