14e301c9e56eaa7bd137783b0a2216afbc8f7d74
tjeffries <scipio.jet@gmail.com>
2014-12-24 04:26:23 -0500

    fool-proofed connection to server (no longer crashed gracelessly) and added unsorted list file name as second Client argument

--- Client.java
+++ Client.java
@@ -57,15 +57,16 @@ public class Client {
   }
 
   public static void main(String [] args){
-    if(args.length != 1){
-      System.out.println("Usage: arg0 = port number");
+    if(args.length != 2){
+      System.out.println("Usage: arg0 = port number, arg1 = source file name");
       System.exit(1);
     }
     try {
+      try {
       Socket soc = new Socket("localhost", Integer.parseInt(args[0]));
       //System.out.println("created socket");
       objectOut = new ObjectOutputStream(soc.getOutputStream());
-      ArrayList<Integer> list = readRandomFile("random_numbers");
+      ArrayList<Integer> list = readRandomFile(args[1]);
       System.out.println("Unsorted List:\n"+list);
       sendArrayList(list);
       objectIn = new ObjectInputStream(soc.getInputStream());
@@ -73,6 +74,10 @@ public class Client {
       ArrayList<Integer> sortedlist = getArrayList();
       System.out.println("Sorted List:\n"+sortedlist);
       writeRandomFile("sorted_numbers", list);
+      } catch(ConnectException ce) {
+        System.out.println("Failed to connect. Check port compatibility, make sure Server is running");
+        System.exit(1);
+      }
     } catch(Exception e) {
       e.printStackTrace();
     }
